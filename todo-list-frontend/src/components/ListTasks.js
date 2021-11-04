fbcefc3f80f3f187956e4e5dece368531affd28e
Flavio Franco <flavio.win@gmail.com>
2021-11-04 09:22:36 -0300

    implementacao: colocando evento de onCLick nas listas de tarefas

--- todo-list-frontend/src/components/ListTasks.js
+++ todo-list-frontend/src/components/ListTasks.js
@@ -3,18 +3,33 @@ import { ListGroup } from 'react-bootstrap';
 
 function ListTasks() {
   const [tasks, setTasks] = useState([]);
+  const [id, setId] = useState('');
+  const [background, setBackground] = useState({ id: '', color: '' });
 
   useEffect(() => {
     fetch('http://localhost:3001/')
       .then((response) => response.json())
       .then((data) => setTasks(data));
-  }, []);
+  }, [tasks]);
+
+  const handleClick = (listId) => {
+    setId(listId);
+    setBackground({ id, color: 'grey' });
+  };
 
   return (
     <ListGroup>
-      {console.log(tasks)}
-      {tasks.map((task, index) => (
-        <ListGroup.Item key={ index }>{task.task}</ListGroup.Item>
+      {tasks.map((task) => (
+        <ListGroup.Item
+          key={ task.id }
+          style={ background.id === task.id
+            ? { backgroundColor: background.color }
+            : null }
+          id={ task.id }
+          onClick={ () => handleClick(task.id) }
+        >
+          {task.task}
+        </ListGroup.Item>
       ))}
     </ListGroup>
   );
